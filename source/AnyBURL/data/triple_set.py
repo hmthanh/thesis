a98e97bb82ae9c50f4f3902b178aaeedcc92224e
MinhTamPhan <phanminhtam247@gmail.com>
2020-06-24 00:15:01 +0700

    add read data

--- source/AnyBURL/data/triple_set.py
+++ source/AnyBURL/data/triple_set.py
@@ -2,5 +2,22 @@ class TripleSet (object):
 
   def __init__(self):
     self.triples = []
-    self.headToList = []
-    self.relationToList = []
+
+    self.headToList = {}
+    self.tailToList = {}
+    self.relationToList = {}
+
+    self.headRelation2Tail = {}
+    self.headTail2Relation = {}
+    self.tailRelation2Head = {}
+
+    self.frequentRelations = set([])
+    # self.readTriples(filepath)
+		#self.indexTriples()
+
+  def read_triples(self, filepath):
+    with open(filepath) as f:
+      lines = f.readlines()
+
+    for line in lines:
+      print(line)
