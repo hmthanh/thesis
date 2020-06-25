5f51ae790d6c359b442e89b22da5bdf94eb7f48d
MinhTamPhan <phanminhtam247@gmail.com>
2020-06-25 19:33:42 +0700

    release atom

--- source/AnyBURL/structure/atom.py
+++ source/AnyBURL/structure/atom.py
@@ -22,6 +22,49 @@ class Atom(object):
     self.left_c = len(self.left) != 1
     self.right_c = len(self.right) != 1
 
+  def contains(self, term):
+    return self.left == term or self.right == term
+
+  def more_special(self, other):
+    if isinstance(other, self.__class__):
+      if self.relation == other.relation:
+        return True
+
+        if self.left == other.left:
+          if not other.right_c and self.right_c:
+            return True
+          return False
+
+        if self.right == other.right:
+          if not other.left_c and self.left_c:
+            return True
+          return False
+        if not other.left_c and not other.right_c and self.left_c and self.right_c:
+            return True
+        return False
+    else:
+      return False
+
+  def is_LRC(self, left_not_right):
+    if left_not_right:
+      return self.left_c
+    return self.right_c
+
+  def clone(self):
+    return Atom(left=self.left, relation=self.relation, right=self.right, left_c=self.left_c, right_c=self.right_c)
+
+  def replace_by_variable(self, constant='', variable=''):
+    count = 0
+    if self.left_c and self.left == constant:
+      self.left_c = False
+      self.left = variable
+      count += 1
+    if self.right_c and self.right == constant:
+      self.right_c = False
+      self.right = variable
+      count += 1
+    return count
+
   def __str__(self):
     return '{} ({}, {})'.format(self.relation, self.left, self.right)
 
