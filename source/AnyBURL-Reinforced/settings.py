70f3a5a9450c0835ea5fccc0198fc9372a262cda
MinhTamPhan <phanminhtam247@gmail.com>
2020-07-23 23:55:21 +0700

    add data train

--- source/AnyBURL-Reinforced/settings.py
+++ source/AnyBURL-Reinforced/settings.py
@@ -2,3 +2,9 @@ class Settings(object):
 
   def __init__(self):
     pass
+  
+  '''*
+	* Do not change this. For APPLY its required that this is set to false, for LEARNING both is okay
+	*'''
+  REWRITE_REFLEXIV = False
+  REWRITE_REFLEXIV_TOKEN = 
