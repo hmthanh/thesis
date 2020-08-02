e3e805ec89e33fcd6a80ed4b3227e46c296c279e
MinhTamPhan <phanminhtam247@gmail.com>
2020-08-02 13:02:07 +0700

    add scorer reinforced

--- source/AnyBURL-Reinforced/learn_reinforced.py
+++ source/AnyBURL-Reinforced/learn_reinforced.py
@@ -1,8 +1,35 @@
 import logging
+from settings import Settings
 
 class LearnReinforced(object):
+  available_threads = set()
+  active = True
+	report = False
+	active_thread = []
+  finished = False
+  stats = []
+  dice = None
 
   def __init__(self):
     logging.basicConfig(filename='learn.log',level=logging.DEBUG)
     logging.info('======================= start new section ======================')
     logging.info('Started LearnReinforced')
+
+  def are_all_available():
+    return True if len(LearnReinforced.available_threads) == Settings.load_learning_config()['WORKER_THREADS'] else False
+
+  def push_thread(id):
+    LearnReinforced.available_threads.add(id)
+
+  def is_active(thread_id, stored_rules, created_rules, produced_score, cyclic, length):
+    if LearnReinforced.active:
+      return True
+    if not LearnReinforced.report:
+      return True
+    else if LearnReinforced.active_thread[thread_id]:
+      type_str = Dice.encode(cyclic, length)
+      stats[thread_id][0] = stored_rules
+			stats[thread_id][1] = created_rules
+      dice.add_score(type_str, producedScore)
+      activeThread[thread_id] = False
+      ### todo
--- source/AnyBURL-Reinforced/threads/scorer_reinforced.py
+++ source/AnyBURL-Reinforced/threads/scorer_reinforced.py
@@ -1,16 +1,40 @@
 from threading import Thread
+from time import sleep
 
 from structure.path import Path
 from algorithm.path_sampler import PathSampler
 from data.triple_set import TripleSet
+from settings import Settings
+from learn_reinforced import LearnReinforced
 
 
 class ScorerReinforced(Thread):
-  def __init__(self, triples, thread_ID='thread_ID'):
+  def __init__(self, triples, thread_id=1):
     threading.Thread.__init__(self)
-    self.thread_ID = thread_ID
+    self.thread_id = thread_id
     self.triples = triples
     self.sampler = PathSampler(triples)
+    self.mine_param_cyclic = True
+    self.mine_param_length = 1
+    self.ready = True
+    self.only_XY = False
+
+  def set_search_parameters(self, cyclic, length):
+    self.mine_param_cyclic = cyclic
+    self.mine_param_length = length
+    self.ready = True
+    self.only_XY = False
+    if self.mine_param_cyclic:
+      if self.mine_param_length > Settings.load_learning_config()['MAX_LENGTH_GROUNDED_CYCLIC']:
+        self.only_XY = True
+
 
   def run(self):
-    print(str(self.thread_name) +"  "+ str(self.thread_ID));
+    while not LearnReinforced.are_all_available():
+      LearnReinforced.push_thread(self.id)
+      sleep(20 / 1000)
+    print('THREAD-{} starts to work with L = {} C = {} '.format(self.thread_id, self.mine_param_length, self.mine_param_cyclic))
+    done = False
+    while not done:
+
+    print(str(self.thread_name) +"  "+ str(self.thread_id))
